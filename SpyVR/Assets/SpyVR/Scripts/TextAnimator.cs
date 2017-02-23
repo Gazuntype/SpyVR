d3ce7f33d5fb58959af3fafc70d011c0a4f3afc2
Olutade Ajiboye <mrbigheaddd@gmail.com>
2017-02-23 15:23:27 +0100

    feat: completed text animation script.

---
+++ SpyVR/Assets/SpyVR/Scripts/TextAnimator.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextAnimator : MonoBehaviour {
+	[Tooltip("Write the text that should be animated.")]
+	public string text;
+
+	[Tooltip("Total time of the animation.")]
+	public float animationTime;
+
+	public Text textComponent;
+
+	// Use this for initialization
+
+	void Start () {
+		StartCoroutine(AnimateText());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator AnimateText()
+	{
+		for (int i = 0; i < text.Length; i++)
+		{
+			if (i == 0)
+			{
+				textComponent.text = text[i].ToString();
+			}
+			else {
+				textComponent.text += text[i];
+			}
+			yield return new WaitForSeconds(animationTime / text.Length);
+		}
+	}
+}
