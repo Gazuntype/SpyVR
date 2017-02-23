ea22d2ab3125fbd49bdb20d7c7e08bd13dc6a29f
Olutade Ajiboye <mrbigheaddd@gmail.com>
2017-02-23 14:45:04 +0100

    feat: color changing for canvas

---
+++ SpyVR/Assets/SpyVR/Scripts/UIControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIControl : MonoBehaviour {
+	public Material glowMaterial;
+	public Image backgroundImage;
+
+	Color glowColor;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		glowColor = glowMaterial.GetColor("_EmissionColor");
+		glowColor.a = 0.5f;
+		backgroundImage.color = glowColor;
+	}
+}
