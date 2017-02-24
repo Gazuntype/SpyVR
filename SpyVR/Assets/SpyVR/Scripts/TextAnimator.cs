7984f6b6edeba887d528496e194443d0a8a5b7c1
Olutade Ajiboye <mrbigheaddd@gmail.com>
2017-02-24 15:05:53 +0100

    feat: imported font; edited text animator script
    A new font was imported and the text animator script was edited to include an initial delay.

--- SpyVR/Assets/SpyVR/Scripts/TextAnimator.cs
+++ SpyVR/Assets/SpyVR/Scripts/TextAnimator.cs
@@ -3,23 +3,38 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class TextAnimator : MonoBehaviour {
-	[Tooltip("Write the text that should be animated.")]
-	public string text;
 
 	[Tooltip("Total time of the animation.")]
 	public float animationTime;
 
+	[Range(0,10)]
+	[Tooltip("The amount of time to delay before beginning the animation.")]
+	public float initialDelay = 5;
+
+	[Tooltip("The UI Text component that shall display the animation.")]
 	public Text textComponent;
 
+	[TextArea]
+	[Tooltip("The string that should be animated")]
+	public string text;
+
+	bool delayComplete;
 	// Use this for initialization
 
 	void Start () {
-		StartCoroutine(AnimateText());
+		delayComplete = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (initialDelay > 0 && !delayComplete)
+		{
+			initialDelay -= Time.deltaTime;
+		}
+		else {
+			StartCoroutine(AnimateText());
+			delayComplete = true;
+		}
 	}
 
 	IEnumerator AnimateText()
